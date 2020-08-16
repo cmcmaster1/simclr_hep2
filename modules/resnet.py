2c2e8dd429a8bc968e58e76bfcbaa741fcd67ba4
cmcmaster1 <mcmastc1@gmail.com>
2020-08-16 21:57:28 +1000

    first commit

---
+++ modules/resnet.py
@@ -0,0 +1,11 @@
+import torchvision
+from fastai2.vision.all import *
+
+def get_resnet(name, pretrained=False):
+    resnets = {
+        "resnet18": xresnet18(pretrained=pretrained),
+        "resnet50": xresnet50(pretrained=pretrained),
+    }
+    if name not in resnets.keys():
+        raise KeyError(f"{name} is not a valid ResNet version")
+    return resnets[name]
