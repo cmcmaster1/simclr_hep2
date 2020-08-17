ff2e3154e5891b4793b1a386a2dd50ef3867892b
cmcmaster1 <mcmastc1@gmail.com>
2020-08-17 15:24:04 +1000

    add more transforms

--- modules/transformations/simclr.py
+++ modules/transformations/simclr.py
@@ -17,6 +17,8 @@ class TransformsSimCLR:
             [
                 torchvision.transforms.RandomResizedCrop(size=size),
                 torchvision.transforms.RandomHorizontalFlip(),  # with 0.5 probability
+                torchvision.transforms.RandomVerticalFlip(),
+                torchvision.transforms.RandomAffine(degrees=15),
                 torchvision.transforms.RandomApply([color_jitter], p=0.8),
                 torchvision.transforms.RandomGrayscale(p=0.2),
                 torchvision.transforms.ToTensor(),
